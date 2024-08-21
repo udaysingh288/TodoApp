99024ea5563c288ab5995df2f469a5fdf544eaf8
udaysingh288 <udaysingh.288@gmail.com>
2024-08-20 23:39:12 -0400

    app changes with html website functionality

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,27 +1,27 @@
-from database import Base
+from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
 class Users(Base):
     __tablename__ = 'users'
-    id = Column(Integer,primary_key=True,autoincrement=True,index=True)
-    email = Column(String(50), nullable=False,unique=True)
-    username = Column(String(50),nullable=False,unique=True)
-    firstname = Column(String(50), nullable=False)
-    lastname = Column(String(50), nullable=False)
-    hashed_password = Column(String(50),nullable=False)
-    is_active = Column(Boolean,default=True)
-    role=Column(String(50),nullable=False)
 
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    phone_number = Column(String)
 
 
 class Todos(Base):
-    __tablename__='todos'
-    id = Column(Integer,primary_key=True,index=True)
-    title=Column(String)
-    description=Column(String)
-    priority=Column(Integer)
-    complete=Column(Boolean,default=False)
-    owner_id=Column(Integer,ForeignKey('users.id'))
+    __tablename__ = 'todos'
 
-
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
